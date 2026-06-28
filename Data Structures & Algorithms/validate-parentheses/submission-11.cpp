5c2d8165815aa8df197a0301167b2f03a804407b
Sophie Nguyen <sofie123sophie@gmail.com>
2026-06-27 22:45:33 -0400

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c: s) {
+            if (c == '(' || c == '[' || c == '{') {
+                st.push(c);
+            }
+            else {
+                if (st.empty()) return false;
+                char top = st.top();
+                if ((c == ')' && top == '(') ||
+                    (c == ']' && top == '[') ||
+                    (c == '}' && top == '{')) {
+                    st.pop();
+                }
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
